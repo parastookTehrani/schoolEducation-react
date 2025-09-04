3a31419ba3e11bcb3a95b1ba14e5e4f10ae35bda
took <poti.tehrani@gmail.com>
2025-09-05 00:58:55 +0330

    feat: main 1st section (benefits)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Header } from "./components/header"
+import { Main } from "./components/main"
 import { Nav } from "./components/nav"
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <Nav />
       <Header />
+      <Main />
     </>
   )
 }
---
+++ src/components/main.jsx
@@ -0,0 +1,11 @@
+import { Benefits } from "./benefits";
+import style from "./style.module.css";
+
+
+export const Main = () => {
+    return(
+        <main className={style.container}>
+         <Benefits />
+        </main>
+    )
+}
